229bd490373f523987ef87445a6436284c08f1f1
JLuis Avalos <luizavalos40@gmail.com>
2023-03-22 10:53:32 -0500

    nav styles done for now

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,32 @@
 import Logo from "../../assets/logo.png";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import "./nav.css";
+import { useState } from "react";
 function Nav() {
+  const [showMobileNaw, setShowMobileNav] = useState(false);
+
   return (
     <nav className="navigation-menu">
       <div className="navigation-menu--mobile">
-        <Bars3Icon className="navigation-menu--icon" />
+        <Bars3Icon
+          className="navigation-menu--icon"
+          onClick={() => setShowMobileNav(!showMobileNaw)}
+        />
         <img src={Logo} alt="logo of shoes" />
       </div>
-      <ul className="navigation-menu--links">
+
+      <ul
+        className={`navigation-menu--links  ${
+          showMobileNaw
+            ? "navigation-menu--links-show"
+            : "navigation-menu--links-hide"
+        }`}
+      >
         <li>Nike</li>
         <li>Jordans</li>
         <li>Adidas</li>
+
+        <img width={80} src={Logo} alt="logo of shoes" />
       </ul>
     </nav>
   );
