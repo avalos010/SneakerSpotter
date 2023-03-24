bf953a1217cb83ba693c225ce0de4bc3a873eeb5
JLuis Avalos <luizavalos40@gmail.com>
2023-03-24 14:31:14 -0500

    update api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL:
+    "https://serverless-netlify--snazzy-empanada-ab1114.netlify.app/.netlify/functions/api/",
+});
+
 export async function getPopularSneakers() {
-  const res = await axios.get("http://localhost:4000/popular");
+  const res = await api.get(`/popular`);
   const data = res.data;
   return data;
 }
 
 export async function searchSneakers(query) {
-  const res = await axios.get(`http://localhost:4000/search/${query}`);
+  const res = await api.get(`/search/${query}`);
   const data = res.data;
   return data;
 }
