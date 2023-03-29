3f17b081cfe29c35f459253049199a738863ecb5
JLuis Avalos <luizavalos40@gmail.com>
2023-03-29 08:54:45 -0500

    remove only

--- cypress/e2e/sneakers.cy.js
+++ cypress/e2e/sneakers.cy.js
@@ -5,18 +5,18 @@ context("Sneakers", () => {
     cy.visit("http://localhost:5173");
   });
 
-  it.only("loads up the sneakers page", () => {
+  it("loads up the sneakers page", () => {
     cy.visit("http://localhost:5173/popular");
     cy.get('[data-cy="sneakers-page"]').should("be.visible");
   });
 
-  it.only("shows all neccesary information in the popular page", () => {
+  it("shows all neccesary information in the popular page", () => {
     cy.visit("http://localhost:5173/popular");
     cy.get('[data-cy="sneaker-price"]').should("be.visible");
     cy.get('[data-cy="sneaker-image"]').should("be.visible");
   });
 
-  it.only("shows all neccesary information in any sneakers page", () => {
+  it("shows all neccesary information in any sneakers page", () => {
     cy.visit("http://localhost:5173/sneakers/nike");
     cy.get('[data-cy="sneaker-price"]').should("be.visible");
     cy.get('[data-cy="sneaker-image"]').should("be.visible");
